9b74761246cc8a70fab572a7b45c0a4a14d87594
Jonathan Pavkov <jpavkov@users.noreply.github.com>
2024-01-17 16:24:58 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# array = np.array([1, 2, 3])
+# print(array)
+# print(type(array))
+
+# array = np.array([[1, 2, 3], [4, 5, 6]])
+# print(array)
+# print(array.shape)
+
+# array = np.zeros((4, 4), dtype=int)
+# print(array)
+
+
+# array = np.ones((4, 4), dtype=int)
+# print(array)
+
+# array = np.full((4, 4), 5, dtype=int)
+# print(array)
+
+# array = np.random.random((5, 5))
+# print(array)
+# print(array[0, 0])
+
+# print(array > 0.5)
+# print(array[array > 0.2])
+# print(np.sum(array))
+# print(np.floor(array))
+# print(np.ceil(array))
+# print(np.round(array))
+
+# first = np.array([1, 2, 3])
+# second = np.array([1, 3, 6])
+# print(first + second)
+# print(first * second)
+# print(first + 2)
+
+dimensions_inch = np.array([1, 2, 3])
+dimensions_cm = dimensions_inch * 2.54
+print(dimensions_cm)
